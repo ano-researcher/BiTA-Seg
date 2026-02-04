eb4541dd0a741c7b958137abf9499cf5a3340eac
ano-researcher <anoresearch79@gmail.com>
2026-02-04 17:32:40 +0400

    dataset_loader.py

--- src/data/dataset_loader.py
+++ src/data/dataset_loader.py
@@ -1,15 +1,22 @@
 import os
 import cv2
 import torch
+import numpy as np
 from torch.utils.data import Dataset
 
 
 class ImageMaskDataset(Dataset):
-    def __init__(self, root, transforms=None):
+    def __init__(self, root, transforms=None, img_size=256):
         self.img_dir = os.path.join(root, "images")
         self.mask_dir = os.path.join(root, "masks")
-        self.files = sorted(os.listdir(self.img_dir))
+
+        self.files = sorted([
+            f for f in os.listdir(self.img_dir)
+            if f.endswith((".png", ".jpg", ".jpeg"))
+        ])
+
         self.transforms = transforms
+        self.img_size = img_size
 
     def __len__(self):
         return len(self.files)
@@ -17,16 +24,21 @@ class ImageMaskDataset(Dataset):
     def __getitem__(self, idx):
         name = self.files[idx]
 
-        img = cv2.imread(os.path.join(self.img_dir, name), 0)
-        mask = cv2.imread(os.path.join(self.mask_dir, name), 0)
+        img = cv2.imread(os.path.join(self.img_dir, name), cv2.IMREAD_GRAYSCALE)
+        mask = cv2.imread(os.path.join(self.mask_dir, name), cv2.IMREAD_GRAYSCALE)
 
-        img = img.astype("float32") / 255.0
-        mask = (mask > 128).astype("float32")
+        img = cv2.resize(img, (self.img_size, self.img_size))
+        mask = cv2.resize(mask, (self.img_size, self.img_size))
 
-        img = torch.tensor(img).unsqueeze(0)
-        mask = torch.tensor(mask).unsqueeze(0)
+        img = img.astype(np.float32) / 255.0
+        mask = (mask > 128).astype(np.float32)
 
         if self.transforms:
-            img, mask = self.transforms(img, mask)
+            augmented = self.transforms(image=img, mask=mask)
+            img = augmented["image"]
+            mask = augmented["mask"]
+
+        img = torch.from_numpy(img).unsqueeze(0)
+        mask = torch.from_numpy(mask).unsqueeze(0)
 
         return img, mask
